0bd3751689c3e99d7063657297596c997ab791ca
gnitulescu <gabriela.nitulescu@infor.com>
2022-12-03 09:00:04 +0100

    7: loading products inside container via MFP

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,11 +1,20 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 module.exports = {
     mode: 'development',
     devServer: {
         port: 8080
     },
-    plugins:[
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container', //not used in the case of the container
+            filename: 'remoteEntry.js',
+            remotes: {products: 'products@http://localhost:8081/remoteEntry.js'}
+            /** products key is referenced by products/ProductsIndex,
+             * products inside the string matches the name in products web.config
+             * url for remote entry file*/
+        }),
         new HtmlWebpackPlugin({
-        template: './public/index.html'
-    })]
+            template: './public/index.html'
+        })]
 }
